d0e719b08db3743436929c21187b404788ade65c
jesch21 <johneschbacher21@gmail.com>
2025-02-18 14:01:03 -0500

    correct changes

--- assets/scripts/home-scripts.js
+++ assets/scripts/home-scripts.js
@@ -221,11 +221,11 @@ const videos = [
         title: "Rogue One: A Star Wars Story"
     },
             Garrett
+    */
     {
         src: 'assets/videos/revenge-trailer.mp4',
         title: "Star Wars: Revenge of the Sith"
     },
-    */
     {
         src: 'assets/videos/heat-trailer.mp4',
         title: "Heat"
